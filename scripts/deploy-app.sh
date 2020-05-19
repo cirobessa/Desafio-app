1bd20014f10487bd943855b5c3fdb70e0fda44ac
cirobessa <cirobessa@yahoo.com>
2020-05-18 21:57:27 -0300

    pequenos-ajustes

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -6,7 +6,29 @@
 ## retira o Taint do master node Kubernete
 sudo kubectl taint nodes --all node-role.kubernetes.io/master-
 
+### INSTALACAO DA AWS-CLI v2
 
-# Trem nao pronto!
-sl -la
+# Baixa da AWS
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
+# move antiga para outro lugar
+sudo mv /usr/bin/aws /home/ciro/aws-OLD-BIN-CLI
+
+# Descompacta
+unzip awscliv2.zip
+
+# Roda instalacao
+ sudo ./aws/install
+
+# gera link no PATH
+sudo ln -s /usr/local/bin/aws /usr/bin/aws
+
+
+### RODA TERRAFORM gerando ambiente ECS-FARGATE na AWS
+cd ../terraform-AWS-ECS-deploy
+
+terraform apply -auto-approve
+
+
+### DESATIVA APLICACOES TERRAFORM e AMBIENTE ECS-FARGATE
+terraform destroy -auto-approve 
